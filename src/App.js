c74c83ed917aa3cc63fc25f3219dcf1f8d3799cc
YonDee <im.yondee@gmail.com>
2019-10-24 15:57:58 +0800

    refactor: modified the component of 'App.js', Now, we can click grid show landmine

--- src/App.js
+++ src/App.js
@@ -1,32 +1,47 @@
 import React from 'react';
 import './App.css';
-import Landmine from'./landmine'
+import './font_1313421_br608omz75m/iconfont.css'
 
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      items: []
+      currentIndex: 0,
+      grids: Array(81).fill(<div className="full-squares"></div>)
     }
-    this.setting = this.setting.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
 
-  setting(number){
-    for(var i=0; i<number; i++){
-      this.state.items.push(
-        <div key={i} className="item" >
-          {/* <Landmine /> */}
+  renderItem(i){
+  }
+
+  handleClick(e, i){
+    let grids = this.state.grids.slice();
+    grids[i] = <i className="iconfont iconzhadan"></i>
+    this.setState({
+      grids: grids
+    })
+  }
+
+
+  render(){
+    let items = []
+    for(var index=0; index<81; index++){
+      const i = index;
+      items.push(
+        <div key={'item-' + i} className="item" onClick={(e) => this.handleClick(e, i)}>
+          {/* <Grid index={i} currentIndex={this.state.currentIndex} /> */}
+          {this.state.grids[i]}
         </div>
       )
     }
-  }
-  render(){
-    this.setting(81)
     return (
       <div className="App">
+        {/* 需要独立出一个父级组件来控制，理解 children */}
         <div id="checkerboard">
-          {this.state.items}
+          {items}
         </div>
+        <span><label>当前点击的组件：</label>{ this.state.currentIndex }</span>
       </div>
     );
   }
