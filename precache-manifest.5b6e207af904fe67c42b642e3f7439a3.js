ebf239bbf48e4171c2b64825fc07ad8bc9405d1d
YonDee <im.yondee@gmail.com>
2019-11-21 09:26:22 +0800

    Updates

---
+++ precache-manifest.5b6e207af904fe67c42b642e3f7439a3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9414a7f6c30721d21d93f62a804634b2",
+    "url": "/mine-sweeping/index.html"
+  },
+  {
+    "revision": "18ddf7a0ad9d44fcf359",
+    "url": "/mine-sweeping/static/css/main.6592d5c2.chunk.css"
+  },
+  {
+    "revision": "e2032bb783119c4f19d2",
+    "url": "/mine-sweeping/static/js/2.d96daa08.chunk.js"
+  },
+  {
+    "revision": "18ddf7a0ad9d44fcf359",
+    "url": "/mine-sweeping/static/js/main.7cbffb3c.chunk.js"
+  },
+  {
+    "revision": "5330578cf37e4e0eb155",
+    "url": "/mine-sweeping/static/js/runtime-main.8ab9e005.js"
+  },
+  {
+    "revision": "62db3b877eb2181ac00806b489b43165",
+    "url": "/mine-sweeping/static/media/PressStart2P.62db3b87.woff"
+  },
+  {
+    "revision": "095883f11d446e2c908c2f5a75dacb5a",
+    "url": "/mine-sweeping/static/media/iconfont.095883f1.ttf"
+  },
+  {
+    "revision": "109c4ca03e40618c54cfb5c8c114c278",
+    "url": "/mine-sweeping/static/media/iconfont.109c4ca0.woff"
+  },
+  {
+    "revision": "6b78e2b63df3b1a310b67595c7e7e25e",
+    "url": "/mine-sweeping/static/media/iconfont.6b78e2b6.svg"
+  },
+  {
+    "revision": "d01481de5bdf80fc0e9004aad3eaec30",
+    "url": "/mine-sweeping/static/media/iconfont.d01481de.eot"
+  }
+]);
